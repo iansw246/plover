0cb3cf3f649acf3696bf60f10a51379cc6af0704
iansw246 <35947093+iansw246@users.noreply.github.com>
2025-07-07 13:59:47 -0700

    Refactor Python wayland implementation

--- keymap_test/wayland_implementation.py
+++ keymap_test/wayland_implementation.py
@@ -1,4 +1,6 @@
 import array
+import collections
+import mmap
 import os
 import socket
 import struct
@@ -43,52 +45,73 @@ def recv_fds_exact(s: socket.socket, length: int, fd_count: int):
     return buffer, fds
 
 def round_up(value: int, multiple: int):
+    """Round `value` up to the nearest multiple of `multiple`.
+    `multiple` must be positive and a power of 2"""
     assert multiple & 1 == 0
     return (value + multiple - 1) & ~(multiple - 1)
 
-wayland_display = os.environ.get("WAYLAND_DISPLAY", "wayland-0")
-xdg_runtime_dir = os.environ.get("XDG_RUNTIME_DIR", "")
-socket_path = os.path.join(xdg_runtime_dir, wayland_display)
+class WaylandConnection:
+    wayland_socket: socket.socket
+    fd_queue: collections.deque[int]
 
-with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
-    s.connect(socket_path)
+    def __init__(self):
+        self.fd_queue = collections.deque()
 
-    # wl_display::get_registry
-    # display id: 1
-    # length: 12 bytes
-    # opcode: 1
-    # new id for register: 2
-    length_and_opcode = (12 << 16) | 1
-    message = struct.pack("=III", DISPLAY_ID, length_and_opcode, REGISTRY_ID)
-    s.sendall(message)
+    def __enter__(self):
+        wayland_display = os.environ.get("WAYLAND_DISPLAY", "wayland-0")
+        xdg_runtime_dir = os.environ.get("XDG_RUNTIME_DIR", "")
+        socket_path = os.path.join(xdg_runtime_dir, wayland_display)
 
-    # wl_display::sync
-    # display id: 1
-    # length: 12 bytes
-    # opcode: 0
-    # new_id for callback: 3
-    length_and_opcode = (12 << 16) | 0
-    message = struct.pack("=III", DISPLAY_ID, length_and_opcode, SYNC_ID)
-    s.sendall(message)
+        self.wayland_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.wayland_socket.connect(socket_path)
 
-    done = False
-    while not done:
-        event_header_bytes = recv_exact(s, WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.wayland_socket.close()
+
+    def recv_message(self):
+        MAX_FD_COUNT = 1
+        event_header_bytes, fds = recv_fds_exact(self.wayland_socket, WAYLAND_MESSAGE_HEADER_SIZE_BYTES, MAX_FD_COUNT)
+        self.fd_queue.extend(fds)
         object_id, length_and_opcode = struct.unpack("=II", event_header_bytes)
         length = length_and_opcode >> 16
+        assert length % 4 == 0, "Length of message must be a multiple of 4."
         opcode = length_and_opcode & 0xFFFF
+        event_data_bytes, fds = recv_fds_exact(self.wayland_socket, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES, MAX_FD_COUNT)
+        self.fd_queue.extend(fds)
+        return object_id, length, opcode, event_data_bytes
+
+    def send_message(self, object_id: int, opcode: int, data: bytes | bytearray):
+        length = WAYLAND_MESSAGE_HEADER_SIZE_BYTES + len(data)
+        # Wayland messages are streams of 32-bit (4 byte) values
+        assert length % 4 == 0, "Length of message must be a multiple of 4."
+        length_and_opcode = (length << 16) | opcode
+        message = struct.pack("=II", object_id, length_and_opcode)
+        self.wayland_socket.sendall(message)
+        self.wayland_socket.sendall(data)
+
+with WaylandConnection() as connection:
+    # wl_display::get_registry
+    # display id: DISPLAY_ID
+    # opcode: 1
+    # new id for register: REGISTRY_ID
+    connection.send_message(DISPLAY_ID, 1, struct.pack("=I", REGISTRY_ID))
+
+    # wl_display::sync
+    # display id: DISPLAY_ID
+    # opcode: 0
+    # new_id for callback: SYNC_ID
+    connection.send_message(DISPLAY_ID, 0, struct.pack("=I", SYNC_ID))
+
+    while True:
+        object_id, length, opcode, event_data_bytes = connection.recv_message()
         if object_id == SYNC_ID:
             # wl_callback::done
             assert opcode == 0
-            # Skip callback_data
-            recv_exact(s, 4)
-            done = True
-            print("Done receiving global objects")
-            continue
-        elif object_id == REGISTRY_ID:
+            break
+        elif object_id == REGISTRY_ID and opcode == 0:
             # wl_registry::global
-            assert opcode == 0
-            event_data_bytes = recv_exact(s, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
             name, interface_length = struct.unpack("=II", event_data_bytes[:8])
             # -1 to skip null terminator
             interface = event_data_bytes[8:8 + interface_length - 1].decode("utf-8")
@@ -98,118 +121,77 @@ with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
 
             if interface == "wl_seat":
                 seat_name = event_data_bytes
-                # # Bind to seat
-                length = (WAYLAND_MESSAGE_HEADER_SIZE_BYTES + len(seat_name) + 4)
-                length_and_opcode = (length << 16) | 0
-                message = struct.pack("=II", REGISTRY_ID, length_and_opcode) + seat_name + struct.pack("=I", SEAT_ID)
-                assert len(message) == length
-                print("Binding seat", message)
-                s.sendall(message)
-
-                # Bind to seat
-                # length = WAYLAND_MESSAGE_HEADER_SIZE_BYTES + 8
-                # length_and_opcode = (length << 16) | 0
-                # message = struct.pack("=IIII", REGISTRY_ID, length_and_opcode, name, SEAT_ID)
-                # assert len(message) == length
-                # print("Sending bind request", message)
-                # s.sendall(message)
-
-                # wl_display::sync
-                # display id: 1
-                # length: 12 bytes
-                # opcode: 0
-                # new_id for callback: 3
-                # length_and_opcode = (12 << 16) | 0
-                # message = struct.pack("=III", DISPLAY_ID, length_and_opcode, SYNC_ID)
-                # s.sendall(message)
-
+                # Bind to seat using wl_registry::bind
+                # opcode 0
+                # the new_id arg follows custom serialization rules (interface name, version, id). See the representation of new_id in https://wayland.freedesktop.org/docs/html/ch04.html#sect-Protocol-Wire-Format
+                # new id for seat: SEAT_ID
+                data = seat_name + struct.pack("=I", SEAT_ID)
+                connection.send_message(REGISTRY_ID, 0, data)
         else:
-            raise ValueError(f"Unknown object id: {object_id}")
+            print("Ignoring event for object", object_id, "opcode", opcode)
 
-    done = False
     has_keyboard = False
-    while not done:
-        event_header_bytes = recv_exact(s, WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
-        object_id, length_and_opcode = struct.unpack("=II", event_header_bytes)
-        length = length_and_opcode >> 16
-        opcode = length_and_opcode & 0xFFFF
+    while True:
+        object_id, length, opcode, event_data_bytes = connection.recv_message()
         if object_id == SYNC_ID:
             # wl_callback::done
             assert opcode == 0
             # Skip callback_data
-            recv_exact(s, 4)
-            done = True
             print("Done receiving seat events")
-            continue
-        elif object_id == SEAT_ID:
+            break
+        elif object_id == SEAT_ID and opcode == 0:
             # wl_seat::capabilities
-            if opcode != 0:
-                print("Ignoring seat event", opcode)
-                recv_exact(s, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
-                continue
+            assert opcode == 0
 
             assert length == WAYLAND_MESSAGE_HEADER_SIZE_BYTES + 4, f"Expected enum to be 4 bytes, got {length}"
-            event_data_bytes = recv_exact(s, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
             capabilities = struct.unpack("=I", event_data_bytes[:4])[0]
             print(f"Capabilities: {capabilities}")
-            if capabilities & 2:
-                # wl_seat::get_keyboard
-                has_keyboard = True
+            has_keyboard = capabilities & 2
             break
-        elif object_id == DISPLAY_ID:
-            if opcode == 0:
-                # wy_display::error
-                raise RuntimeError(f"Wayland error: {repr(recv_exact(s, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES).decode('utf-8'))}")
-            if opcode == 1:
-                # wl_display::delete_id
-                assert length == WAYLAND_MESSAGE_HEADER_SIZE_BYTES + 4
-                event_data_bytes = recv_exact(s, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
-                id_num = struct.unpack("=I", event_data_bytes[:4])[0]
-                print(f"Deleted id: {id_num}")
-            else:
-                raise ValueError(f"Unknown opcode: {opcode}")
+        elif object_id == DISPLAY_ID and opcode == 0:
+            # wl_display::error
+            raise RuntimeError(f"Wayland error: {repr(event_data_bytes)}")
+        elif object_id == DISPLAY_ID and opcode == 1:
+            # wl_display::delete_id
+            assert length == WAYLAND_MESSAGE_HEADER_SIZE_BYTES + 4
+            id_num = struct.unpack("=I", event_data_bytes[:4])[0]
+            print(f"Deleted id: {id_num}")
         else:
-            raise ValueError(f"Unknown object id: {object_id}. Length: {length}, opcode: {opcode}")
+            print("Ignoring event for object", object_id, "opcode", opcode)
 
     if not has_keyboard:
         raise ValueError("No keyboard")
 
     # Get wl_keyboard
-    length_and_opcode = (12 << 16) | 1
-    message = struct.pack("=III", SEAT_ID, length_and_opcode, KEYBOARD_ID)
-    s.sendall(message)
+    connection.send_message(SEAT_ID, 1, struct.pack("=I", KEYBOARD_ID))
 
+    fds: list[int] = []
     # Wait for wl_keyboard::keymap
     while True:
+        object_id, length, opcode, event_data_bytes = connection.recv_message()
+        if object_id == KEYBOARD_ID and opcode == 0:
+            # wl_keyboard::keymap
+            assert length == WAYLAND_MESSAGE_HEADER_SIZE_BYTES + 8
 
-        event_header_bytes, fds = recv_fds_exact(s, WAYLAND_MESSAGE_HEADER_SIZE_BYTES, 1)
-        object_id, length_and_opcode = struct.unpack("=II", event_header_bytes)
-        length = length_and_opcode >> 16
-        opcode = length_and_opcode & 0xFFFF
-        if object_id == KEYBOARD_ID:
-            if opcode == 0:
-                # wl_keyboard::keymap
-                assert length == WAYLAND_MESSAGE_HEADER_SIZE_BYTES + 8
+            keymap_format, keymap_size = struct.unpack("=II", event_data_bytes)
+            print(f"Keymap format: {keymap_format}")
+            if keymap_format != 1:
+                raise ValueError(f"Unsupported keymap format: {keymap_format}")
 
-                event_data_bytes, new_fds = recv_fds_exact(s, 8, 1)
-                fds += new_fds
-                keymap_format, keymap_size = struct.unpack("=II", event_data_bytes)
-                print(f"Keymap format: {keymap_format}")
-                if keymap_format != 1:
-                    raise ValueError(f"Unsupported keymap format: {keymap_format}")
+            fd = connection.fd_queue.popleft()
 
-                if not fds:
-                    raise ValueError("No keymap fd received")
+            if not fd:
+                raise ValueError("No keymap fd received")
 
-                print(f"Keymap size: {keymap_size}")
+            print(f"Keymap size: {keymap_size}")
                 
-                xkb_context = xkb.Context()
-                with open(fds[0], "rb") as keymap_file:
-                    keymap = xkb_context.keymap_new_from_file(keymap_file)
-
-            else:
-                print("Ignoring keyboard event", opcode)
-                recv_exact(s, length - WAYLAND_MESSAGE_HEADER_SIZE_BYTES)
+            xkb_context = xkb.Context()
+            with mmap.mmap(fd, keymap_size, flags=mmap.MAP_PRIVATE, prot=mmap.PROT_READ) as keymap_file:
+                keymap = xkb_context.keymap_new_from_file(keymap_file)
+                
+            syms = keymap.key_get_syms_by_level(26, 1, 0)
+            if syms:
+                print("Keycode 26 symbols:", *(xkb.keysym_get_name(sym) for sym in syms), sep=" ")
+            break
         else:
-            raise ValueError(f"Unknown object id: {object_id}")
-
+            print("Ignoring event for object", object_id, "opcode", opcode)

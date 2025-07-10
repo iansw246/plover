e25d8b3ebb57ae5e974ecde942a2d9bdd8276a00
iansw246 <35947093+iansw246@users.noreply.github.com>
2025-07-09 22:14:46 -0700

    Revert "Add alternate modifier find script loop order"
    This reverts commit 0aa8be938819f11601d76f77eaf9b0a8a0b8b728.
    
    The previous loop order is faster according to benchmarks

--- keymap_test/xkb_modifier_keycodes.py
+++ keymap_test/xkb_modifier_keycodes.py
@@ -5,30 +5,28 @@ context = xkb.Context()
 keymap = context.keymap_new_from_names(layout="gb")
 num_mods = keymap.num_mods()
 
-for mod_index in range(0, num_mods):
-    mod_name = keymap.mod_get_name(mod_index)
-    print("==Mod name==", mod_name)
+for keycode in keymap:
+    keyboard_state = xkb.KeyboardState(keymap)
+    key_state = keyboard_state.update_key(keycode, xkb.KeyDirection.XKB_KEY_DOWN)
 
+    is_key_mod = (key_state & xkb.StateComponent.XKB_STATE_MODS_DEPRESSED) and not ((key_state & xkb.StateComponent.XKB_STATE_MODS_LOCKED) or (key_state & xkb.StateComponent.XKB_STATE_MODS_LATCHED))
+    if not is_key_mod:
+        continue
 
-    for keycode in keymap:
-        keyboard_state = xkb.KeyboardState(keymap)
-        key_state = keyboard_state.update_key(keycode, xkb.KeyDirection.XKB_KEY_DOWN)
+    num_layouts = keymap.num_layouts_for_key(keycode)
+    for layout in range(0, num_layouts):
 
-        is_key_mod = (key_state & xkb.StateComponent.XKB_STATE_MODS_DEPRESSED) and not ((key_state & xkb.StateComponent.XKB_STATE_MODS_LOCKED) or (key_state & xkb.StateComponent.XKB_STATE_MODS_LATCHED))
-        if not is_key_mod:
+        layout_is_active = keyboard_state.layout_index_is_active(layout, xkb.StateComponent.XKB_STATE_LAYOUT_EFFECTIVE)
+
+        if not layout_is_active:
             continue
 
-        is_mod_active = keyboard_state.mod_index_is_active(mod_index, xkb.StateComponent.XKB_STATE_MODS_DEPRESSED)
-        if not is_mod_active:
-            continue
-
-        num_layouts = keymap.num_layouts_for_key(keycode)
-        for layout in range(0, num_layouts):
-
-            layout_is_active = keyboard_state.layout_index_is_active(layout, xkb.StateComponent.XKB_STATE_LAYOUT_EFFECTIVE)
-
-            if not layout_is_active:
+        for mod_index in range(0, num_mods):
+            is_mod_active = keyboard_state.mod_index_is_active(mod_index, xkb.StateComponent.XKB_STATE_MODS_DEPRESSED)
+            if not is_mod_active:
                 continue
+
+            mod_name = keymap.mod_get_name(mod_index)
 
             keysyms = keymap.key_get_syms_by_level(keycode, layout, 0)
 
@@ -36,6 +34,7 @@ for mod_index in range(0, num_mods):
                 continue
 
             keysym_name = xkb.keysym_get_name(keysyms[0])
+            print("==Mod name==", mod_name)
             print("Layout name", keymap.layout_get_name(layout))
             print("Keycode", keycode)
             print("Keysym name", keysym_name)

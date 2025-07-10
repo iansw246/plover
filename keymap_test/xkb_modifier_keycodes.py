1737b3882db3a37510b2cf9340fd6fb76c2be9e7
iansw246 <35947093+iansw246@users.noreply.github.com>
2025-07-09 21:48:25 -0700

    Test script to get keycode for each modifier

---
+++ keymap_test/xkb_modifier_keycodes.py
@@ -0,0 +1,41 @@
+from xkbcommon import xkb
+
+context = xkb.Context()
+
+keymap = context.keymap_new_from_names(layout="gb")
+num_mods = keymap.num_mods()
+
+for mod_index in range(0, num_mods):
+    mod_name = keymap.mod_get_name(mod_index)
+    print("===Mod name===", mod_name)
+
+
+    for keycode in keymap:
+        keyboard_state = xkb.KeyboardState(keymap)
+        key_state = keyboard_state.update_key(keycode, xkb.KeyDirection.XKB_KEY_DOWN)
+
+        num_layouts = keymap.num_layouts_for_key(keycode)
+        for layout in range(0, num_layouts):
+
+            layout_is_active = keyboard_state.layout_index_is_active(layout, xkb.StateComponent.XKB_STATE_LAYOUT_EFFECTIVE)
+
+            if layout_is_active:
+                for mod_index in range(0, num_mods):
+
+                    is_key_mod = (key_state & xkb.StateComponent.XKB_STATE_MODS_DEPRESSED) and not ((key_state & xkb.StateComponent.XKB_STATE_MODS_LOCKED) or (key_state & xkb.StateComponent.XKB_STATE_MODS_LATCHED))
+                    if is_key_mod:
+
+                        is_mod_active = keyboard_state.mod_index_is_active(mod_index, xkb.StateComponent.XKB_STATE_MODS_DEPRESSED)
+                        if is_mod_active:
+                            if keymap.mod_get_name(mod_index) == mod_name:
+
+                                keysyms = keymap.key_get_syms_by_level(keycode, layout, 0)
+
+                                if len(keysyms) != 1:
+                                    continue
+
+                                keysym_name = xkb.keysym_get_name(keysyms[0])
+                                print("Layout name", keymap.layout_get_name(layout))
+                                print("Keycode", keycode)
+                                print("Keysym name", keysym_name)
+                                print("Key state", key_state)

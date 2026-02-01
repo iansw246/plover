1cbe82e6c285ce8f839c60a70bae4af487f29f5f
iansw246 <35947093+iansw246@users.noreply.github.com>
2026-02-01 07:54:55 -0600

    Hide Linux keyboard layout help label when not on Linux

--- plover/gui_qt/config_window.py
+++ plover/gui_qt/config_window.py
@@ -335,6 +335,7 @@ class ConfigOption:
         # Other widgets to be added immediately after the main widget_class
         # Does not work in dependents
         self.additional_widget_classes = additional_widget_classes
+        self.additional_widgets = []
 
 
 class ConfigWindow(QDialog, Ui_ConfigWindow, WindowStateMixin):
@@ -624,7 +625,9 @@ class ConfigWindow(QDialog, Ui_ConfigWindow, WindowStateMixin):
                 option.label.setBuddy(option.widget)
                 layout.addRow(option.label, option.widget)
                 for additional_widget_class in option.additional_widget_classes:
-                    layout.addRow(None, additional_widget_class())
+                    additional_widget = additional_widget_class()
+                    layout.addRow(None, additional_widget)
+                    option.additional_widgets.append(additional_widget)
             frame = QFrame()
             frame.setLayout(layout)
             frame.setAccessibleName(section)
@@ -648,6 +651,8 @@ class ConfigWindow(QDialog, Ui_ConfigWindow, WindowStateMixin):
             if keyboard_layout_option is not None:
                 keyboard_layout_option.label.hide()
                 keyboard_layout_option.widget.hide()
+                for additional_widget in keyboard_layout_option.additional_widgets:
+                    additional_widget.hide()
 
         # temporary hiding start_minimized setting on macOS due to bug in
         # macOS 26, see https://github.com/openstenoproject/plover/issues/1782
